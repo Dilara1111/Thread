54885f65db84f2e6b9e68a43f20caea132baea10
HuseynovaDilara <huseynova.8885@gmail.com>
2023-11-30 19:56:33 +0400

    Task added

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks; 
+
+namespace MultiThreading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(PrintText);
+            thread.Start();
+        }
+        static void PrintText()
+        {
+            string a = "Salam";
+            for (int i = 0; i < a.Length; i++)
+            {
+                Thread.Sleep(1000);
+                Console.Write(a[i]);
+            }
+        }
+    }
+}
